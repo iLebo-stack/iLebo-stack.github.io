be0243ba2a4d1bf7e767c2b48437289746c8f8e1
Sibiya S <mogane420@gmail.com>
2021-08-11 03:05:31 +0200

    responsive navbar

--- Menu.js
+++ Menu.js
@@ -3,10 +3,13 @@ btn = document.getElementById("menuIcon");
 navItems = document.getElementById("navItems");
 
 function menuBtn() {
-    if (navItems.style.display == "flex") {
-        navItems.style.display = "none";
-    } else 
-    navItems.style.display = "flex";
+    if (navItems.className == 'navlinks') {
+        navItems.className = 'navlinks ' + 'responsive';
+        btn.className = 'toggle';
+    } else {
+        navItems.className = 'navlinks';
+        btn.className = '';
+    }
 }
 
 btn.addEventListener("click", menuBtn, false);
